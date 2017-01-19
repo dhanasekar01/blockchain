948563b16bc46dfcaf8510e7dfe7011ec6bce614
dhanasekar01 <dhanalovin@gmail.com>
2017-01-19 14:47:00 +0530

    Cattle information

--- chaincode.go
+++ chaincode.go
@@ -3,12 +3,22 @@ package main
 import (
 	"errors"
 	"fmt"
-
+	"encoding/json"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"strconv"
 )
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
+}
+
+
+type Cattle struct{
+	CattleId string `json:"cattleId"`
+	CattleTag string `json:"cattleTag"`
+	Description string `json:"description"`
+	Weight float64 `json:"weight"`
+	FarmerId string `json:"farmerId"`
 }
 
 func main() {
@@ -41,6 +51,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
 		return t.write(stub, args)
+	} else if function == "createCattle" {
+		return t.createCattle(stub,args)
+	} else if function == "getCattle" {
+		return t.createCattle(stub,args)
 	}
 	fmt.Println("invoke did not find func: " + function)
 
@@ -77,6 +91,46 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 		return nil, err
 	}
 	return nil, nil
+}
+
+func (t *SimpleChaincode) createCattle(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var err error
+	fmt.Println("Initializing Cattle Creation")
+
+	weight, err := strconv.ParseFloat(args[3], 64)
+
+	cattle := Cattle{
+		CattleId : args[0],
+		CattleTag : args[1],
+		Description : args[2],
+		Weight : weight,
+		FarmerId : args[4],
+	}
+
+	bytes, err := json.Marshal(&cattle)
+	if err != nil {
+		return nil, err
+	}
+	
+	err = stub.PutState(cattle.CattleId, bytes)
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil	
+}
+
+// read cattle
+func (t *SimpleChaincode) getCattle(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
 }
 
 // read - query function to read key/value pair
